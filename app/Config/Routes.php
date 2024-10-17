55707459cc9065b35046e33cabd5a8161cac16fa
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-10-17 14:28:14 +0700

    Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,11 +7,18 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
+// Visitor?Member - Belajar Ekspor
 $routes->get('/belajar-ekspor', 'KomunitasEkspor::belajar_ekspor');
 $routes->get('/belajar-ekspor-detail', 'KomunitasEkspor::belajar_ekspor_detail');
+
+// Visitor - Pendaftaran
 $routes->get('/pendaftaran', 'KomunitasEkspor::pendaftaran');
+$routes->post('daftar-member', 'KomunitasEkspor::registrasiMember');
+
+// Visitior?Member - Video Tutorial
 $routes->get('/video-tutorial', 'KomunitasEkspor::video_tutorial');
 $routes->get('/video-tutorial-detail', 'KomunitasEkspor::video_tutorial_detail');
-$routes->post('daftar-member', 'KomunitasEkspor::registrasiMember');
+
+// Member - Data Member
 $routes->get('/data-member', 'KomunitasEkspor::data_member');
-$routes->get('/detail-member', 'KomunitasEkspor::detail_member');
+$routes->get('/detail-member/(:any)', 'KomunitasEkspor::detail_member/$1');
