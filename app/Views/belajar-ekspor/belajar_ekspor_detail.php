dfa46347bdca831a01a1e4575911f7e2e8bc216b
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-10-21 15:02:21 +0700

    button belajar ekspor detail

--- app/Views/belajar-ekspor/belajar_ekspor_detail.php
+++ app/Views/belajar-ekspor/belajar_ekspor_detail.php
@@ -199,7 +199,7 @@
                 <div class="badgepanjang p-3">
                     <div class="d-flex justify-content-between align-items-center mt-3 mb-3 flex-md-row flex-column">
                         <h5 class="kategori font-weight-bold mb-0 text-left text-md-left">Gabung Member Yuk <br>Untuk Akses 100%</h5>
-                        <a href="/pendaftaran" class="btn mt-md-0">Pendaftaran Member</a>
+                        <a href="/pendaftaran" class="btn btn-custom mt-md-0">Pendaftaran Member</a>
                     </div>
                 </div>
             </div>
