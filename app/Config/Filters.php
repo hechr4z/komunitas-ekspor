918303398d2cc7db3f1585621afe83c025809ead
zainidineafi <firmanmangkuraja@gmail.com>
2024-10-22 11:09:39 +0700

    Upload filter, navbar route , pendaftaran , data-member

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -34,6 +34,7 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'languagefilter' => \App\Filters\LanguageFilter::class,
     ];
 
     /**
@@ -53,6 +54,7 @@ class Filters extends BaseFilters
         'before' => [
             'forcehttps', // Force Global Secure Requests
             'pagecache',  // Web Page Caching
+            'languagefilter',
         ],
         'after' => [
             'pagecache',   // Web Page Caching
