06e6f34db62d7cfb8a3e5a66ea2fc4f42e53079b
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-10-28 18:39:45 +0700

    fix user img in edit profile

--- app/Views/member/edit-profile.php
+++ app/Views/member/edit-profile.php
@@ -170,7 +170,7 @@
     <div class="card p-4 shadow-sm mt-5">
         <!-- Image at the top -->
         <div class="text-center mb-2" style="width: 250px; height: 250px; margin: auto; overflow: hidden; border-radius: 50%; position: relative;">
-            <img src="/img/slider-1.jpg" class="img-fluid" alt="" style="width: 100%; height: 100%; object-fit: cover; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+            <img src="<?= base_url('img/' . $member['foto_profil']); ?>" class="img-fluid" alt="" style="width: 100%; height: 100%; object-fit: cover; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
         </div>
         <h4 class="text-center mt-1"><?= $member['username'] ?></h4>
         <!-- Tabs Navigation -->
