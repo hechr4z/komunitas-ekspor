a114c574614a5e38f6cfdbe58e2f8497e155af69
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-11-01 14:20:06 +0700

    fix migration satuan

--- app/Database/Migrations/2024-10-21-021227_CreateSatuanTable.php
+++ app/Database/Migrations/2024-10-21-021227_CreateSatuanTable.php
@@ -15,17 +15,19 @@ class CreateSatuanTable extends Migration
                 'unsigned'       => true,
                 'auto_increment' => true,
             ],
-            // maybe id member?
+            'id_member' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
             'satuan' => [
                 'type'       => 'VARCHAR',
                 'constraint' => 255,
             ],
         ]);
 
-        // Set primary key
         $this->forge->addKey('id_satuan', true);
-
-        // Create the table
+        $this->forge->addForeignKey('id_member', 'member', 'id_member', 'CASCADE', 'CASCADE');
         $this->forge->createTable('satuan');
     }
 
