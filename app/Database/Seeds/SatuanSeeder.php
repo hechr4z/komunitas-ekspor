8f0b33e7dda7c9dde6b6c6f84e9d8f55af3267c6
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-11-01 14:20:17 +0700

    fix satuan seeder

--- app/Database/Seeds/SatuanSeeder.php
+++ app/Database/Seeds/SatuanSeeder.php
@@ -8,13 +8,20 @@ class SatuanSeeder extends Seeder
 {
     public function run()
     {
-        $data = [
-            [
-                'id_satuan' => 1,
-                'satuan' => 'pcs',
-            ]
-        ];
+        // Ambil semua id_member dari tabel member
+        $members = $this->db->table('member')->select('id_member')->get()->getResultArray();
 
+        // Siapkan data yang akan dimasukkan ke tabel satuan
+        $data = [];
+        foreach ($members as $member) {
+            $data[] = [
+                'id_satuan' => null, // Jika id_satuan auto-increment, bisa diisi null atau dihilangkan
+                'id_member' => $member['id_member'],
+                'satuan' => 'pcs',
+            ];
+        }
+
+        // Insert batch ke tabel satuan
         $this->db->table('satuan')->insertBatch($data);
     }
 }
