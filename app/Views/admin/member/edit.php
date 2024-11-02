8fcb3735a109c4c1b854d62cc6b00d1497b0a4a1
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-11-02 16:33:10 +0700

    fixing back button 4

--- app/Views/admin/member/edit.php
+++ app/Views/admin/member/edit.php
@@ -89,7 +89,7 @@
 
                             <div class="mb-3">
                                 <button type="submit" class="btn btn-primary">Simpan</button>
-                                <a href="/admin-data-member" class="btn btn-secondary">Kembali</a>
+                                <a href="<?= base_url('admin-member') ?>" class="btn btn-secondary">Kembali</a>
                             </div>
                         </form>
                     </div>
