d12447a3b27fce4b1bf0a54504bd28e93b0d61da
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-11-02 11:32:17 +0700

    fix admin member in navbar

--- app/Views/admin/layout/navbar.php
+++ app/Views/admin/layout/navbar.php
@@ -25,8 +25,17 @@
                             </svg>
                         </span>
                         <span class="nav-link-text">Dashboard</span>
-                    </a><!--//nav-link-->
-                </li><!--//nav-item-->
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link <?= (current_url() == base_url('admin-member')) ? 'active' : '' ?>" href="<?= base_url('admin-member') ?>">
+                        <span class="nav-icon">
+                            <i class="fa-solid fa-user"></i>
+                        </span>
+                        <span class="nav-link-text">Member</span>
+                    </a>
+                </li>
 
                 <li class="nav-item">
                     <a class="nav-link <?= (current_url() == base_url('admin-buyers')) ? 'active' : '' ?>" href="<?= base_url('admin-buyers') ?>">
@@ -34,17 +43,6 @@
                             <i class="fa-solid fa-cart-shopping"></i>
                         </span>
                         <span class="nav-link-text">Buyers</span>
-                    </a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link <?= (current_url() == base_url('admin/user/index')) ? 'active' : '' ?>" href="<?= base_url('admin/user/index') ?>">
-                        <span class="nav-icon">
-                            <svg width="16" height="16" fill="currentColor" class="fas fa-portrait bi bi-activity" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z" />
-                            </svg>
-                        </span>
-                        <span class="nav-link-text">Admin Jago</span>
                     </a>
                 </li>
 
