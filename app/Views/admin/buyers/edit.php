9d1d166595d72fc375b5becef4997debe7b8804a
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-11-03 11:27:08 +0700

    idk too

--- app/Views/admin/buyers/edit.php
+++ app/Views/admin/buyers/edit.php
@@ -3,37 +3,36 @@
 
 <div class="app-content pt-3 p-md-3 p-lg-4">
     <div class="container-xl">
-        <h1 class="app-page-title">Edit Data Member</h1>
+        <h1 class="app-page-title">Ubah Buyers</h1>
         <hr class="mb-4">
         <div class="row g-4 settings-section">
             <div class="col-12 col-md-8">
                 <div class="app-card app-card-settings shadow-sm p-4">
                     <div class="card-body">
-                        <form action="#" method="post" enctype="multipart/form-data">
+                        <form action="<?= base_url('admin-update-buyers/' . $buyers['id_buyers']) ?>" method="post" enctype="multipart/form-data">
                             <div class="mb-3">
                                 <label class="form-label">Nama Perusahaan</label>
-                                <input type="text" class="form-control" name="nama_perusahaan" value="johnsmith Corp"
-                                    required>
+                                <input type="text" class="form-control" name="nama_perusahaan" value="<?= $buyers['nama_perusahaan'] ?>" required>
                             </div>
 
                             <div class="mb-3">
                                 <label class="form-label">Negara</label>
-                                <input type="text" class="form-control" name="negara" value="Indonesia" required>
+                                <input type="text" class="form-control" name="negara_perusahaan" value="<?= $buyers['negara_perusahaan'] ?>" required>
                             </div>
 
                             <div class="mb-3">
                                 <label class="form-label">Kode HS</label>
-                                <input type="text" class="form-control" name="kode_hs" value="150" required>
+                                <input type="text" class="form-control" name="hs_code" value="<?= $buyers['hs_code'] ?>" required>
                             </div>
 
                             <div class="mb-3">
                                 <label class="form-label">Email</label>
-                                <input type="email" class="form-control" name="email" value="saya@gmail.com" required>
+                                <input type="email" class="form-control" name="email_perusahaan" value="<?= $buyers['email_perusahaan'] ?>" required>
                             </div>
 
                             <div class="mb-3">
                                 <label class="form-label">Website</label>
-                                <input type="url" class="form-control" name="website" value="saya.com" required>
+                                <input type="text" class="form-control" name="website_perusahaan" value="<?= $buyers['website_perusahaan'] ?>" required>
                             </div>
 
                             <div class="mb-3">
