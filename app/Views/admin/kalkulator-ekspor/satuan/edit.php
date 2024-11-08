00cf3d6950c070e3235f49a15ad12e5575af41c7
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-11-08 13:11:00 +0700

    admin edit satuan views

--- app/Views/admin/kalkulator-ekspor/satuan/edit.php
+++ app/Views/admin/kalkulator-ekspor/satuan/edit.php
@@ -3,33 +3,26 @@
 
 <div class="app-content pt-3 p-md-3 p-lg-4">
     <div class="container-xl">
-        <h1 class="app-page-title" style="color: #03AADE;">Ubah Satuan</h1>
+        <h1 class="app-page-title" style="color: #03AADE;">Ubah Data Satuan</h1>
         <hr class="mb-4">
         <div class="row g-4 settings-section">
             <div class="col-12 col-md-8">
                 <div class="app-card app-card-settings shadow-sm p-4">
                     <div class="card-body">
-                        <form action="#" method="post" enctype="multipart/form-data">
+                        <form action="<?= base_url('admin-update-satuan/' . $satuan['id_satuan']) ?>" method="post" enctype="multipart/form-data">
                             <div class="mb-3">
-                                <label class="form-label">Nama Member</label>
-                                <select class="form-select" id="nama_member" name="nama_member" required>
-                                    <option value="" disabled>Pilih Nama Member</option>
-                                    <option value="1" selected>Member 1</option>
-                                    <option value="2">Member 2</option>
-                                    <option value="3">Member 3</option>
-                                    <!-- Tambahkan opsi lainnya jika diperlukan -->
-                                </select>
+                                <label class="form-label">Username Member</label>
+                                <input type="text" class="form-control" id="id_member" name="id_member" value="<?= $satuan['id_member'] ? $member[array_search($satuan['id_member'], array_column($member, 'id_member'))]['username'] : '' ?>" disabled>
                             </div>
 
                             <div class="mb-3">
                                 <label class="form-label">Satuan</label>
-                                <input type="text" class="form-control" name="satuan" value="PCS" required>
+                                <input type="text" class="form-control" name="satuan" value="<?= $satuan['satuan'] ?>" required>
                             </div>
 
                             <div class="mb-3">
-                                <button type="submit" class="btn text-white"
-                                    style="background-color: #03AADE;">Simpan</button>
-                                <a href="/admin-satuan" class="btn btn-secondary">Kembali</a>
+                                <button type="submit" class="btn text-white" style="background-color: #03AADE;">Simpan</button>
+                                <a href="<?= base_url('admin-satuan') ?>" class="btn btn-secondary">Kembali</a>
                             </div>
                         </form>
                     </div>
