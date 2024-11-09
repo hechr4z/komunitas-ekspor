009671f9b583957b605da6ebab4b13d3bedd8815
Muh. Rafqi Putra <kenb.space3112@gmail.com>
2024-11-09 11:48:47 +0700

    kelayakan bisnis blog id

--- app/Language/id/Blog.php
+++ app/Language/id/Blog.php
@@ -11,6 +11,7 @@ return [
     'headerApp1' => 'Kalkulator Harga Ekspor',
     'headerApp2' => 'Monitoring Progres Pemasaran',
     'headerApp3' => 'Audit Website',
+    'headerApp4' => 'Kelayakan Bisnis',
     'headerBuyers' => 'Data Buyers',
     //endnavbar
 
@@ -188,6 +189,7 @@ return [
     'wantToOpenApp1' => 'Mau Buka Aplikasi Kalkulator Harga Ekspor?',
     'wantToOpenApp2' => 'Mau Buka Aplikasi Monitoring Progres Pemasaran?',
     'wantToOpenApp3' => 'Mau Buka Aplikasi Audit Website?',
+    'wantToOpenApp4' => 'Mau Buka Aplikasi Kelayakan Bisnis?',
 
     'letsGoRegisterSA' => 'Yuk Daftar Member Dulu!',
     'registerSA' => 'Daftar',
